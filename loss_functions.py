68ef07e0320b4071b1235854ed36cdadd80782c8
ChatterMonkey <basu.maya@icloud.com>
2021-07-11 10:20:32 -0700

    sigloss function added

--- loss_functions.py
+++ loss_functions.py
@@ -7,3 +7,8 @@ def significance_loss(expectedSignal,expectedBackground):
         signalWeight = expectedSignal/torch.sum(y_true)
         backgroundWeight = expectedBackground/torch.sum(1-y_true)
 
+        s = signalWeight*torch.sum(y_pred*y_true)
+        b= backgroundWeight*torch.sum(y_pred*(1-y_true))
+
+        return -(s*s)/(s+b + torch.epsilon())
+    return sigloss
