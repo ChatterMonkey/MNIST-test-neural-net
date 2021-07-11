40e67cd7c2eea9bbbdb46dddfcce718e482b3531
ChatterMonkey <basu.maya@icloud.com>
2021-07-11 10:41:45 -0700

    added sigloss test

--- loss_functions.py
+++ loss_functions.py
@@ -10,5 +10,22 @@ def significance_loss(expectedSignal,expectedBackground):
         s = signalWeight*torch.sum(y_pred*y_true)
         b= backgroundWeight*torch.sum(y_pred*(1-y_true))
 
-        return -(s*s)/(s+b + torch.epsilon())
+        return -(s*s)/(s+b + 0.000000001)
     return sigloss
+
+
+
+
+expectedSignal = 1
+expectedSignal = 9
+y_true = torch.tensor([0,0,1,0,0,0,0,0,0])
+y_pred = torch.tensor([0,1,0,0,0,0,0,0,0])
+
+
+sigloss = significance_loss(1,9)
+
+print(sigloss(y_true,y_pred))
+
+
+
+
