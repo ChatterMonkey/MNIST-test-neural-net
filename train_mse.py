2dd29f2b71cf6d6888bd4db6568493df87bbc218
ChatterMonkey <basu.maya@icloud.com>
2021-07-17 14:23:16 -0700

    MSE training

---
+++ train_mse.py
@@ -0,0 +1,30 @@
+import torch
+from loaders import train_loader
+from variables import *
+import torch.nn.functional
+from functions import prepare_target
+
+
+def train(network, optimizer):
+    network.train()
+    optimizer.zero_grad()
+    train_losses = []
+
+    for batch, (data, target) in enumerate(train_loader):
+
+        target = prepare_target(target) #convert target into 1 for siganl and 0 for background
+        output = torch.reshape(network(data), (-1,)) #query network and reshape output
+        loss = torch.nn.functional.mse_loss(output, target)
+
+        if batch % 10 == 0:
+            train_losses.append(loss.item())
+
+        loss.backward()
+        optimizer.step()
+
+        if batch % log_interval == 0:
+            print("Training batch {}/{}, Loss: {}".format(batch,60000/train_batch_size,loss))
+            #torch.save(network.state_dict(), '/Users/mayabasu/results/model.pth')
+            #torch.save(optimizer.state_dict(), '/Users/mayabasu/results/optimizer.pth')
+
+    return train_losses
