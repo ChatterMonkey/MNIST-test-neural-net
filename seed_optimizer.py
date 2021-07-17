03868c495c0c0f615ac9411e85977aeb11c5bf18
ChatterMonkey <basu.maya@icloud.com>
2021-07-16 20:50:00 -0700

    created seed_optimizer function

--- seed_optimizer.py
+++ seed_optimizer.py
@@ -1,18 +1,28 @@
 import torch
 from net import Net
+from test import test
+import numpy
 
-ave_deviations = []
-i = 7
-deviations = []
-torch.manual_seed(i)
-net = Net()
-test_losses,total_number_correct,true_positive_count,false_positive_count,sample_output = test(net)
-for j in range(len(sample_output)):
-    average = sum(sample_output[j])/10
-    for k in range(10):
-        deviation = abs(sample_output[j][k] - average)
-        deviations.append(deviation)
-print("DEVIATIONS = {}".format(deviations))
-print("average deviations = {}".format(sum(deviations)/len(deviations)))
-ave_deviations.append(sum(deviations)/len(deviations))
-print(ave_deviations)
+
+
+
+def optimize_seed(numberofseeds):
+    ave_deviations_per_seed = []
+    for i in range(numberofseeds):
+        ave_deviations_per_query = []
+        torch.manual_seed(i)
+        net = Net()
+        test_losses,total_number_correct,true_positive_count,false_positive_count,sample_output = test(net)
+        absolute_deviations = []
+        for j in range(len(sample_output)):
+            query_average = sum(sample_output[j])/10
+            for k in range(10):
+                absolute_deviation = abs(sample_output[j][k] - query_average)
+                absolute_deviations.append(absolute_deviation)
+        print("DEVIATIONS = {}".format(absolute_deviation))
+        print("average deviations for each query  = {}".format(sum(absolute_deviations)/len(absolute_deviations)))
+        ave_deviations_per_seed.append(sum(absolute_deviations)/len(absolute_deviations))
+    print(ave_deviations_per_seed)
+    print("the best seed is {}".format(numpy.argmin(ave_deviations_per_seed)))
+
+optimize_seed(10)
