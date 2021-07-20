2ec54042fa871a2cacf589184a7a7b4d88bf3646
ChatterMonkey <basu.maya@icloud.com>
2021-07-19 17:50:20 -0700

    updated

--- variables.py
+++ variables.py
@@ -1,12 +1,12 @@
 
 signal = 4
 
-n_epochs = 3
+n_epochs = 20
 train_batch_size = 4000
 
 test_batch_size = 5000
 
-learning_rate = 0.0001
+learning_rate = 0.01
 momentum = 0.5
 signal_weight_strength = 1
 
