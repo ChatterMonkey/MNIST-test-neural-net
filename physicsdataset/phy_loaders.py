47c2707362172e78d18bcec1ebaaba046319817c
ChatterMonkey <basu.maya@icloud.com>
2021-08-17 17:09:23 -0700

    fixed variable shadowing problem

--- physicsdataset/phy_loaders.py
+++ physicsdataset/phy_loaders.py
@@ -11,25 +11,27 @@ def open_training_data(number_of_batches):
         print("Warning! requested too much training data, only 200000 records available, {} requested".format(number_of_batches * variables.train_batch_size))
         return "warning"
 
-    training_data = np.zeros((variables.train_batch_size,variables.num_variables,number_of_batches), dtype=float) #variables by number of events in one batch by number of batches
-    training_target = np.zeros((variables.train_batch_size,1, number_of_batches))
-
-    with open("training.csv") as training_data:
-        training_data.seek(0)
-        trainreader = csv.reader(training_data)
+    training_data = np.zeros((number_of_batches,variables.train_batch_size,variables.num_variables), dtype=float) #variables by number of events in one batch by number of batches
+    training_target = np.zeros((number_of_batches,variables.train_batch_size,1))
+    print(training_data)
+    with open("training.csv") as training_data_file:
+        training_data_file.seek(0)
+        trainreader = csv.reader(training_data_file)
         next(trainreader)
 
         for batch in tqdm(range(number_of_batches), colour = "black",desc= "Loading Training Data"):
 
             for event in range(variables.train_batch_size):
                 line = next(trainreader)
+                print(line)
+                print(line[0])
                 for variable in range(variables.num_variables):
-                    training_data[event][variable][batch] = line[variable]
+                    training_data[batch,event,variable] = line[variable]
 
                 if line[32]=='s':
-                    training_target[event][0][batch] = 1
+                    training_target[batch,event,0] = 1
                 else:
-                    training_target[event][0][batch] = 1
+                    training_target[batch,event,0] = 0
 
 
         return training_data,training_target
@@ -37,36 +39,29 @@ def open_training_data(number_of_batches):
 
 
 def open_test_data(number_of_batches):
-    number_of_signals = 0
-    number_of_background_events = 0
+
     if number_of_batches * variables.test_batch_size > 50000:
         print("Warning! requested too much testing data, only 50000 records available, {} requested".format(number_of_batches * variables.test_batch_size))
 
-    test_data_lists = []
-    test_target_lists = []
+    testing_data = np.zeros((number_of_batches,variables.train_batch_size,variables.num_variables), dtype=float) #variables by number of events in one batch by number of batches
+    testing_target = np.zeros((number_of_batches,variables.train_batch_size,1))
 
-    with open("training.csv") as testing_data:
-        testreader = csv.reader(testing_data)
+    with open("training.csv") as testing_data_file:
+        testreader = csv.reader(testing_data_file)
         for i in range(200001):
             next(testreader)
 
-        for j in tqdm(range(number_of_batches), colour="black",desc= "Loading Testing Data"):
-            test_data_list = []
-            test_target_list = []
-            for i in range(variables.test_batch_size):
+        for batch in tqdm(range(number_of_batches), colour="black",desc= "Loading Testing Data"):
+            for event in range(variables.test_batch_size):
                 line = next(testreader)
-                test_data_list.append(line[1:31] )
-                if line[32] == 's':
-                    number_of_signals += 1
-                    test_target_list.append(1)
+                for variable in range(variables.num_variables):
+                    testing_data[batch,event,variable] = line[variable]
+
+                if line[32]=='s':
+                    testing_target[batch,event,0] = 1
                 else:
-                    number_of_background_events += 1
-                    test_target_list.append(0)
-            test_data_lists.append(test_data_list)
-            test_target_lists.append(test_target_list)
-        print("")
-        print("s/b is {}/{} = {}%".format(number_of_signals,number_of_background_events,number_of_signals/number_of_background_events*100))
-
-        return test_data_lists,test_target_lists
+                    testing_target[batch,event,0] = 0
+        print(testing_data)
 
 
+        return testing_data,testing_target
