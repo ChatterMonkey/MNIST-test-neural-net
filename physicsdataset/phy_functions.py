afca6bc5800b7987714cf440a1be8b731f6cc83a
ChatterMonkey <basu.maya@icloud.com>
2021-09-06 19:04:17 -0700

    removed rounding from asimov loss

--- physicsdataset/phy_functions.py
+++ physicsdataset/phy_functions.py
@@ -18,9 +18,8 @@ def find_loss(output, target, loss_function_id):
     elif loss_function_id == 2:
         loss = f.binary_cross_entropy(output,target)
     elif loss_function_id == 3:
-
         loss = asimov_loss(output,target)
-        print("loss is {}".format(loss))
+
     else:
         print("LOSS FUNCTION ID NOT VAID")
         return "LOSS FUNCTION ID NOT VAID"
@@ -28,17 +27,27 @@ def find_loss(output, target, loss_function_id):
 
 
 
+#def asimov_significance(output, target):
+#    signalWeight=variables.expectedSignal/torch.sum(target)
+#    bkgdWeight = variables.expectedBackground/torch.sum(1-target)
+
+#    s = signalWeight*torch.sum(torch.round(output)*target)
+#    b = bkgdWeight*torch.sum(torch.round(output)*(1 - target))
+#    sigB = variables.systematic*b
+
+ #   return torch.sqrt(2*((s+b)*torch.log((s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB+0.000001)+0.000001)-b*b*torch.log(1+sigB*sigB*s/(b*(b+sigB*sigB)+0.000001))/(sigB*sigB+0.000001)))
 def asimov_significance(output, target):
     signalWeight=variables.expectedSignal/torch.sum(target)
     bkgdWeight = variables.expectedBackground/torch.sum(1-target)
 
-    s = signalWeight*torch.sum(torch.round(output)*target)
-    b = bkgdWeight*torch.sum(torch.round(output)*(1 - target))
+    s = signalWeight*torch.sum(output*target)
+    b = bkgdWeight*torch.sum(output*(1 - target))
     sigB = variables.systematic*b
 
     return torch.sqrt(2*((s+b)*torch.log((s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB+0.000001)+0.000001)-b*b*torch.log(1+sigB*sigB*s/(b*(b+sigB*sigB)+0.000001))/(sigB*sigB+0.000001)))
 
-def asimov_loss(output,target):
+def asimov_loss(output, target):
+
 
     significance = asimov_significance(output,target)
     return 1/significance
