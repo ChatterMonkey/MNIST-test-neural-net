6c4ede47e19c6bad59d16a86e3f4e591b211d0af
ChatterMonkey <basu.maya@icloud.com>
2021-09-17 21:59:42 -0700

    Completed asimov estimate evaluation graph maker

--- physicsdataset/phy_functions.py
+++ physicsdataset/phy_functions.py
@@ -26,17 +26,6 @@ def find_loss(output, target, loss_function_id):
         return "LOSS FUNCTION ID NOT VAID"
     return loss
 
-
-
-#def asimov_significance(output, target):
-#    signalWeight=variables.expectedSignal/torch.sum(target)
-#    bkgdWeight = variables.expectedBackground/torch.sum(1-target)
-
-#    s = signalWeight*torch.sum(torch.round(output)*target)
-#    b = bkgdWeight*torch.sum(torch.round(output)*(1 - target))
-#    sigB = variables.systematic*b
-
- #   return torch.sqrt(2*((s+b)*torch.log((s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB+0.000001)+0.000001)-b*b*torch.log(1+sigB*sigB*s/(b*(b+sigB*sigB)+0.000001))/(sigB*sigB+0.000001)))
 def asimov_significance(output, target):
 
     signalWeight=variables.expectedSignal/torch.sum(target)
@@ -50,22 +39,29 @@ def asimov_significance(output, target):
 
 
 def discrete_asimov_significance(output, target, cutoff, systematic):
-    print(target)
+
     b = 0
     s = 0
-
-
     for i in range(len(output)):
-        if target(i) == 0:
-            if output(i) > cutoff:
+        if target[i].item() == 0:
+            if (output[i].item() > cutoff) or (output[i].item == cutoff):
                 b += 1
         else:
-            if output(i) > cutoff:
+            if (output[i].item() > cutoff) or (output[i].item == cutoff):
                 s += 1
 
-    sigB = systematic*b
 
-    return torch.sqrt(2*((s+b)*torch.log((s+b)*(b+sigB*sigB)/(b*b+(s+b)*sigB*sigB+0.000001)+0.000001)-b*b*torch.log(1+sigB*sigB*s/(b*(b+sigB*sigB)+0.000001))/(sigB*sigB+0.000001)))
+    #print("{},{}".format(s,b))
+    sigB = systematic*b
+    lnone = ln_oned(s,b,sigB)
+    lntwo = ln_twod(s,b,sigB)
+    #print(lnone)
+    #print(lntwo)
+
+
+    asimov_loss = math.sqrt(2*((s+b)*lnone - (b*b)/(sigB*sigB+ 0.000001)*lntwo))
+    return asimov_loss
+
 
 def asimov_loss(output, target):
 
@@ -76,7 +72,7 @@ def asimov_loss(output, target):
 
 def ln_one(s,b,sigB):
 
-    ln_one = torch.log(((s + b)*(b + sigB*sigB))/( b*b + (s+b)*sigB*sigB+ 0.000001))
+    ln_one = torch.log(((s + b)*(b + sigB*sigB))/( b*b + (s+b)*sigB*sigB+ 0.000001) + 0.00000001)
 
     return ln_one
 def ln_two(s,b,sigB):
@@ -86,3 +82,12 @@ def ln_two(s,b,sigB):
 
 
 
+def ln_oned(s,b,sigB):
+
+    ln_one = math.log(((s + b)*(b + sigB*sigB) + 0.000001)/( b*b + (s+b)*sigB*sigB+ 0.000001) )
+
+    return ln_one
+def ln_twod(s,b,sigB):
+    ln_two = math.log(1 + (sigB*sigB*s)/(b*(b + sigB*sigB)+ 0.000001))
+
+    return ln_two
